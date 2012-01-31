fd4b729c327466ec3ac425ca791592b3be7c1c58
Michael Ostermeier <mosterme@users.noreply.github.com>
2012-01-31 23:56:58 +0000

    - silk icons + jquery ui
    git-svn-id: svn://code/p/joggle/trunk@28 62b0412d-8fe6-4ae7-94e2-c7691c08b19d

--- src/main/webapp/js/joggle.js
+++ src/main/webapp/js/joggle.js
@@ -1,22 +1,31 @@
-﻿/**
- * @author  $Author$
- * @version $Revision$
- */
-$().ready(fetch()); var albums, artists;
-$('#clean').click(function(){$('.cur').parent().prev().prevAll().remove()});
-$('#clear').click(function(){$('.cur').parent().prevAll().remove(); $('.cur').parent().nextAll().remove()});
-$('#back').click(function(){beh()});
-$('#stop').click(function(){peh()});
-$('#next').click(function(){neh()});
-$('#brart').click(function(){browse('artist')});
-$('#bralb').click(function(){browse('album')});
+﻿/* $Id$ */
+var albums, artists; $().ready(go());
+
+function go() {
+	$('#bralb').button({ text: false, icons: { primary: "ui-icon-person"}});
+	$('#brart').button({ text: false, icons: { primary: "ui-icon-disk"}});
+	$('#clean').button({ text: false, icons: { primary: "ui-icon-carat" }});
+	$('#clear').button({ text: false, icons: { primary: "ui-icon-trash" }});
+	$('#back').button({ text: false, icons: { primary:" ui-icon-seek-start"}});
+	$('#next').button({ text: false, icons: { primary: "ui-icon-seek-end"}});
+	fetch();
+	$('#bralb').click(function(){browse('album')});
+	$('#brart').click(function(){browse('artist')});
+	$('#clean').click(function(){$('.cur').parent().prev().prevAll().remove()});
+	$('#clear').click(function(){$('.cur').parent().prevAll().remove(); $('.cur').parent().nextAll().remove()});
+	$('#back').click(function(){beh()});
+	$('#stop').click(function(){peh()});
+	$('#next').click(function(){neh()});
+}
+
+// Z Previous Track // X Play/Restart/Unpause // C Pause/Unpause // V Stop // B Next Track
 
 function beh() { $('.cur').parent().prev().children().first().click() }
 function neh() { $('.cur').parent().next().children().first().click() }
 function peh() { var p = document.getElementById('player'); p.paused ? p.play() : p.pause() }
 
 function fetch() {
-	$.ajax({ dataType: "jsonp", jsonp: "$callback", url: "./search/artist/", success: function(data){ artists = data.d; browse('artist'); } });
+	$.ajax({ dataType: "jsonp", jsonp: "$callback", url: "./search/artist/", success: function(data){ artists = data.d; browse('artist') } });
 	$.ajax({ dataType: "jsonp", jsonp: "$callback", url: "./search/album/",  success: function(data){ albums = data.d } });
 }
 
@@ -26,10 +35,12 @@ function queue(what, keyword) {
 
 function browse(what) {
 	$('#browser').empty(); $('#browser-nav').empty();
-	if (what == 'album') {
+	if (what == 'album') 
+	{ 
 		$('#talb').tmpl(albums).appendTo('#browser'); $('#bralb').hide(); $('#brart').show();
 	}
-	else {
+	else
+	{ 
 		$('#tart').tmpl(artists).appendTo('#browser'); $('#brart').hide(); $('#bralb').show();
 	}
 	$('#browser').listnav({includeAll: false, noMatchText: "nothing", showCounts: false});
